13975a9965d6d9e3887d801078d3438a23b753e8
Jamie Cho <jchocholate@gmail.com>
2019-12-07 12:36:53 -0500

    add omitted packages to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+print('pkg -> {}'.format(find_packages()))
 
 setup(name='cho-util',
       version='0.1',
@@ -7,7 +9,7 @@ setup(name='cho-util',
       author='Jamie Cho',
       author_email='jchocholate@gmail.com',
       license='MIT',
-      packages=['cho_util'],
+      packages=find_packages(),
       zip_safe=False,
       scripts=['bin/pyhelp']
       )

d7a0a41ed667e73a8f4547f09e1daa0e716c3904
Yoonyoung Cho <jchocholate@gmail.com>
2022-03-29 16:41:12 +0900

    downgrade

--- src/cho_util/__init__.py
+++ src/cho_util/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("cho_util")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from . import cam
 from . import math
 from . import viz
